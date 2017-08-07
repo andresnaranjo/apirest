4084c34e8aa23ad2a7898caec6a52bed9ae0da72
andres naranjo <andresnaranjo@afnarqui.com>
2017-08-07 14:08:49 -0500

    ingresar rutas y parametros

--- api-restfull-favoritos/index.js
+++ api-restfull-favoritos/index.js
@@ -12,6 +12,19 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json())
 
+app.get('/prueba/:nombre?', function(req, res) {
+    var nombre = 'indefinido'
+    if (req.params.nombre) {
+        nombre = req.params.nombre
+    }
+
+    res.status(200).send({
+        data: [2, 3, 4],
+        message: 'devolver desde nodejs',
+        name: nombre
+    })
+})
+
 app.listen(port, function() {
     console.log(`Api rest favoritos corriendo por el puerto ${port}`)
     console.log('nodemon corriendo')
