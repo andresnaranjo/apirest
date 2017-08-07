2c825b260a91f7331c11583e4026b64ddd4f4b33
andres naranjo <andresnaranjo@afnarqui.com>
2017-08-07 16:03:44 -0500

    implemento api rest para favoritos

--- api-restfull-favoritos/controllers/favorito.js
+++ api-restfull-favoritos/controllers/favorito.js
@@ -13,6 +13,36 @@ function prueba(req, res) {
     })
 }
 
+function getFavorito(req, res) {
+    var favoritoId = req.params.id
+    res.status(200).send({ mensaje: 'desde la api favorito ' + favoritoId })
+}
+
+function getFavoritos(req, res) {
+    res.status(200).send({ data: 'datos' })
+}
+
+function saveFavorito(req, res) {
+    var params = req.body
+    res.status(200).send({ save: true, favorito: params })
+}
+
+function updateFavorito(req, res) {
+    var params = req.body
+    res.status(200).send({ update: true, favorito: params })
+}
+
+function deleteFavorito(req, res) {
+    var params = req.body
+    res.status(200).send({ delete: true, favorito: params })
+}
+
+
 module.exports = {
-    prueba
+    prueba,
+    getFavorito,
+    getFavoritos,
+    saveFavorito,
+    updateFavorito,
+    deleteFavorito
 }
--- api-restfull-favoritos/routes/favorito.js
+++ api-restfull-favoritos/routes/favorito.js
@@ -7,5 +7,11 @@ var favoritoControllers = require('../controllers/favorito.js')
 var api = express.Router()
 
 api.get('/prueba/:nombre?', favoritoControllers.prueba)
+api.get('/favorito/:id', favoritoControllers.getFavorito)
+api.get('/favorito', favoritoControllers.getFavoritos)
+api.post('/favorito', favoritoControllers.saveFavorito)
+api.put('/favorito/:id', favoritoControllers.updateFavorito)
+api.delete('/favorito/:id', favoritoControllers.deleteFavorito)
+
 
 module.exports = api
